fa8415cfc65ea9260efd422d2ec00a8e69269f82
Anthony McDonald <a@awews.com>
2017-09-23 03:28:46 -0600

    add index tests

--- test/controllers/distance_tracker_controller_test.exs
+++ test/controllers/distance_tracker_controller_test.exs
@@ -8,4 +8,35 @@ defmodule DistanceTracker.TrackerControllerTest do
   def insert_tracker(params \\ []) do
     Factory.insert(Tracker, params)
   end
+
+  test "List all trackers", %{conn: conn} do
+    tracker_a = insert_tracker()
+    tracker_b = insert_tracker()
+
+    %{"data" => data} = conn
+      |> get(tracker_path(conn, :index))
+      |> Map.get(:resp_body)
+      |> Poison.decode!
+
+    assert [
+      %{
+        "activity" => tracker_a.activity,
+        "completed_at" => DateTime.to_iso8601(tracker_a.completed_at),
+        "distance" => tracker_a.distance,
+        "inserted_at" => DateTime.to_iso8601(tracker_a.inserted_at),
+        "updated_at" => DateTime.to_iso8601(tracker_a.updated_at),
+        "uuid" => tracker_a.uuid
+      },
+      %{
+        "activity" => tracker_b.activity,
+        "completed_at" => DateTime.to_iso8601(tracker_b.completed_at),
+        "distance" => tracker_b.distance,
+        "inserted_at" => DateTime.to_iso8601(tracker_b.inserted_at),
+        "updated_at" => DateTime.to_iso8601(tracker_b.updated_at),
+        "uuid" => tracker_b.uuid
+      }
+    ] == data
+  end
+
+  
 end
