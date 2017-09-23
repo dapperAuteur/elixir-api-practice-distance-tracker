821ec554575c2671e33b4093cf0e038fac720711
Anthony McDonald <a@awews.com>
2017-09-23 03:33:13 -0600

    add show test

--- test/controllers/distance_tracker_controller_test.exs
+++ test/controllers/distance_tracker_controller_test.exs
@@ -38,5 +38,23 @@ defmodule DistanceTracker.TrackerControllerTest do
     ] == data
   end
 
-  
+  test "Show a tracker", %{conn: conn} do
+    tracker_a = insert_tracker()
+    insert_tracker()
+
+    %{resp_body: body, status: status} = conn
+      |> get(tracker_path(conn, :show, tracker_a.uuid))
+
+    %{"data" => data} = Poison.decode!(body)
+
+    assert 200 == status
+    assert %{
+      "activity" => tracker_a.activity,
+      "completed_at" => DateTime.to_iso8601(tracker_a.completed_at),
+      "distance" => tracker_a.distance,
+      "inserted_at" => DateTime.to_iso8601(tracker_a.inserted_at),
+      "updated_at" => DateTime.to_iso8601(tracker_a.updated_at),
+      "uuid" => tracker_a.uuid
+    } == data
+  end
 end
