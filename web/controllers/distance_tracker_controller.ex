904d88e2a8cba628ccbd924df297579bcb18a9ff
Anthony McDonald <a@awews.com>
2017-09-23 02:15:06 -0600

    add show method to controller

--- web/controllers/distance_tracker_controller.ex
+++ web/controllers/distance_tracker_controller.ex
@@ -8,4 +8,15 @@ defmodule DistanceTracker.TrackerController do
     trackers = Repo.all(Tracker)
     render(conn, "index.json", trackers: trackers)
   end
+
+  def show(conn, %{"id" => uuid}) do
+    with tracker = %Tracker{} <- Repo.get(Tracker, uuid) do
+      render(conn, "show.json", tracker: tracker)
+    else
+      nil ->
+        conn
+        |> put_status(404)
+        |> render(ErrorView, "404.json", error: "Not found")
+    end
+  end
 end
